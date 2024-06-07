12b5f90c01224a06266cb9d379dea68fd33d20b0
Nicole M Firestone <113856095+nicolemfirestone@users.noreply.github.com>
2024-06-07 16:44:50 -0500

    Update player.py
    Set negative values to 0

--- player.py
+++ player.py
@@ -30,6 +30,10 @@ class Player(object):
         max_focus = 20
         max_knowledge = 25
 
+        self.fun = np.max(0, self.fun) 
+        self.focus = np.max(0, self.focus)
+        self.knowledge = np.max(0, self.knowledge)
+
         (tqdm(range(max_fun), desc="Fun:      ",
               bar_format='{desc:<12}{percentage:3.0f}%|{bar:10}|{n_fmt}/{total_fmt}{bar:-10b}')).update(self.fun)
         (tqdm(range(max_focus), desc="Focus:    ",
