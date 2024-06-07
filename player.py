15ff7281081dd2dda27eeb717528ea4528a7303b
arenbeck <97977525+arenbeck@users.noreply.github.com>
2024-06-07 15:26:32 -0500

    add player class file

---
+++ player.py
@@ -0,0 +1,39 @@
+from tqdm import tqdm
+
+class Player:
+    def __init__(self, name=None, travel_type=None):
+        self.name = name
+        self.travel_type = travel_type
+        self.focus = 0
+        self.fun = 0
+        self.knowledge = 0
+
+        if self.name.lower() == "bryan":
+            print("Hi Bryan! You're bang up to the elephant!")
+        elif self.name.lower() == "adam":
+            print("Hi Adam, at the end of this week you are required to watch High School Musical")
+        elif self.name.lower() == "sabina":
+            print("HAPPY BIRTHDAY WEEK SABINA!!!!1!!")
+
+    # methods
+    def mod_focus(self, num):
+        self.focus += num
+
+    def mod_fun(self, num):
+        self.fun += num
+
+    def mod_knowledge(self, num):
+        self.knowledge += num
+
+    def check_status(self):
+        max_fun = 20
+        max_focus = 20
+        max_knowledge = 25
+
+        (tqdm(range(max_fun), desc="Fun:      ",
+              bar_format='{desc:<12}{percentage:3.0f}%|{bar:10}|{n_fmt}/{total_fmt}{bar:-10b}')).update(self.fun)
+        (tqdm(range(max_focus), desc="Focus:    ",
+              bar_format='{desc:<12}{percentage:3.0f}%|{bar:10}|{n_fmt}/{total_fmt}{bar:-10b}')).update(self.focus)
+        (tqdm(range(max_knowledge), desc="Knowledge:",
+              bar_format='{desc:<12}{percentage:3.0f}%|{bar:10}|{n_fmt}/{total_fmt}{bar:-10b}')).update(self.knowledge)
+
